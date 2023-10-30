25b700fa2366fb706afd2d8d07bf691d55803877
seleon79 <149102469+seleon79@users.noreply.github.com>
2023-10-31 01:18:56 +0200

    Add files via upload

--- mail.php
+++ mail.php
@@ -1,12 +1,18 @@
 <?php
-$to = "seleon79@gmail.com"; // адрес куда отправлять письмо 1
-$subject = "Отправка формы с портфолио"; // заголовок м
-$redirect = "./ok.html"; // адрес страницы, на которую нужно !
+// Если скрытое поле заполнили
+if ($_POST[':']!=''){
+  die('No bots!');
+}
+else{
+  $to = "mail@gmail.com"; // адрес куда отправлять письмо
+$subject = "Отправка формы с сайта уроки английского"; // заголовок письма
+$redirect = "./ok.html"; // адрес страницы, на которую нужно перейти после отправки письма
 
-foreach ($_POST as $key => $value)
-{ $fields .= $key.": "$value." \r\n"; }
-$message = $subject." \r\n".$fields;
-$headers = "Content-type: text/plain; charset=utf-8 \r\n";
-mail (Sto, $subject, $message, $headers);
-header ('Location: '.$redirect);
+foreach($_POST as $key => $value)
+{ $fields .= $key.": ".$value." \r\n"; }
+$message = $subject." \r\n".$fields; 
+$headers  = "Content-type:  text/plain; charset=utf-8 \r\n"; 
+mail($to, $subject, $message, $headers);
+header('Location: '.$redirect);
+}
 ?>
