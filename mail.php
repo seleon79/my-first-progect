cd83a29850b5a6245f4e2fdfef0e1f6e940e2e4f
seleon79 <149102469+seleon79@users.noreply.github.com>
2023-10-30 23:35:00 +0200

    Add files via upload

--- mail.php
+++ mail.php
@@ -5,15 +5,15 @@ $sitename = "https://seleon79.github.io/my-first-progect/mail.php"; //Назва
 //
 $name = trim($_POST["name"]); //Создаем переменную $name и помещаем в неё значения из поля формы 'name'
 $mail = trim($_POST["email"]); //Создаем переменную $mail и помещаем в неё значения из поля формы 'email'
-$mail = trim($_POST["phone"]); //Создаем переменную $mail и помещаем в неё значения из поля формы 'email'
+$phone = trim($_POST["phone"]); //Создаем переменную $phone и помещаем в неё значения из поля формы 'phone'
 //Если надо добавить ещё поля в форму, нужно добавить переменную по примеру 7-й строки и 
 //её же в выражение на 11-й строке
 //
-$message = "\nИмя: $name \nПочта: $mail"; //Создаем шаблон письма
+$message = "\nИмя: $name \nПочта: $mail \nТелефон: $phone"; //Создаем шаблон письма
 $pagetitle = "Новая заявка с сайта \"$sitename\""; //Задаем тему письма
 mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
 if($message){
-    header("Location: https://www.sitename.com/thank-you");  //В случае успешной отправки перенаправляем пользователя на заранее созданную страницу с благодарностью
+    header("Location: https://seleon79.github.io/my-first-progect/mail.php");  //В случае успешной отправки перенаправляем пользователя на заранее созданную страницу с благодарностью
   }else{  //В случае ошибки выводим такое сообщение:
     echo "Упс, что-то пошло не так.";
   }
